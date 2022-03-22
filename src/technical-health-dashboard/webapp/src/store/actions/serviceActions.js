f1b6018d356471a3b02bd088da5f262dadefcf5d
Vasil Velikov <428329@student.fontys.nl>
2022-03-22 13:11:05 +0200

    added env variables for docker-compose.yml

--- src/technical-health-dashboard/webapp/src/store/actions/serviceActions.js
+++ src/technical-health-dashboard/webapp/src/store/actions/serviceActions.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 import {GET_SERVICES} from "../../constants";
 import {envGet} from "../../envHelper";
 
-const SERVER_URL = envGet('SERVER_URL');
+const SERVER_HOST = envGet('SERVER_HOST');
 const SERVER_PORT = envGet('SERVER_PORT');
 
 export const getServices = () => {
     return async dispatch => {
-        const response = await axios.get(`${SERVER_URL}:${SERVER_PORT}/servicestates`)
+        const response = await axios.get(`${SERVER_HOST}:${SERVER_PORT}/servicestates`)
 
         dispatch({type: GET_SERVICES, data: response.data});
     }
