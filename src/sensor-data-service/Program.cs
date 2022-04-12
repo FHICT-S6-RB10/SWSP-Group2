0e071385d34303f5c5b89d3dd9b493a954168408
Crist Rusnac <dcgws21rki@yahoo.com>
2022-04-12 10:32:29 +0200

    post api

--- src/sensor-data-service/Program.cs
+++ src/sensor-data-service/Program.cs
@@ -1,84 +1,94 @@
 using NATS.Client;
+using Microsoft.AspNetCore.Mvc;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddSingleton<MeasurementRepo>();
+
+//var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+//builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+//app.UseRouting();
 
-ConnectionFactory cf = new ConnectionFactory();
-Options opts = ConnectionFactory.GetDefaultOptions();
-opts.Url = "nats://localhost:4222";
+//ConnectionFactory cf = new ConnectionFactory();
+//Options opts = ConnectionFactory.GetDefaultOptions();
+//opts.Url = "nats://localhost:4222";
 
-IConnection c = cf.CreateConnection(opts);
+//IConnection c = cf.CreateConnection(opts);
 
-EventHandler<MsgHandlerEventArgs> h = (sender, args) =>
-{
-    Console.WriteLine($"Received {args.Message}");
-    string receivedMessage = Encoding.UTF8.GetString(args.Message.Data);
-    var deserializedMessage = JsonDocument.Parse(receivedMessage);
-    var decodedMessage = deserializedMessage.RootElement.GetProperty("message").ToString();
-    var origin = deserializedMessage.RootElement.GetProperty("origin").ToString();
+//EventHandler<MsgHandlerEventArgs> h = (sender, args) =>
+//{
+//    Console.WriteLine($"Received {args.Message}");
+//    string receivedMessage = Encoding.UTF8.GetString(args.Message.Data);
+//    var deserializedMessage = JsonDocument.Parse(receivedMessage);
+//    var decodedMessage = deserializedMessage.RootElement.GetProperty("message").ToString();
+//    var origin = deserializedMessage.RootElement.GetProperty("origin").ToString();
 
+//    if (decodedMessage.ToLower() == "ping")
+//    {
+//        var reply = args.Message.Reply;
+//        var replyMessage = Encoding.UTF8.GetBytes("th_status_recieved");
+//        c.Publish(reply, replyMessage);
+//        Console.WriteLine($"Published message {Encoding.UTF8.GetString(replyMessage)} to {reply} ");
+//        //client.PostAsync(uri, new StringContent(jsonInString, Encoding.UTF8, "application/json"));
 
-    if (decodedMessage.ToLower() == "new measurement")
-    {
-        Console.WriteLine("Storing measurement" + origin);
-    }
-};
+//    }
+//};
 
-IAsyncSubscription s = c.SubscribeAsync("raw_data", h);
+//app.MapGet("/measurements", ([FromServices] MeasurementRepo repo) => {
+//    return repo.GetAll();
+//});
 
+app.MapPost("/measurements/", async (Measurement measurement) => {
+
+    Console.WriteLine(measurement);
+    return Results.Created($"/measurements/{measurement.patientId}", measurement);
+});
 
 app.Run();
 
-record Measurement(int id, int userId, JsonContent measurement);
+public class Measurement
+{
+    public string patientId { get; set; }
+    public string patientName { get; set; }
+    public JsonContent jsonContent { get; set; }
 
-internal record Request(string origin, string message);
+    public Measurement(string patientId, string patientName, JsonContent jsonContent)
+    {
+        this.patientId = patientId;
+        this.patientName = patientName;
+        this.jsonContent = jsonContent;
+    }
+}
+//record Measurement(string PatientId, string DeviceId, JsonContent content);
 
-
-
-
-//--------------------------------------------
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//var app = builder.Build();
-
-//app.MapGet("/servicestates", () => new
+//class MeasurementRepo
 //{
-//    name = "SensorDataService",
-//    status = 1
-//});
+//    private readonly List<Measurement> _measurements = new List<Measurement>();
 
-//app.Run();
+//    public void Create(Measurement measurement)
+//    {
+//        if (measurement is null)
+//        {
+//            return;
+//        }
 
-//record State(String name, int status);
+//        _measurements.Add(measurement);
+//    }
 
-//---------------------------------------
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-//builder.Services.AddRazorPages();
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
+//    public List<Measurement> GetByPatientId(string PatientId)
+//    {
+//        return _measurements.FindAll(x => x.PatientId == PatientId);
+//    }
+//    public List<Measurement> GetAll()
+//    {
+//        return _measurements;
+//    }
 //}
 
-//app.UseHttpsRedirection();
-//app.UseStaticFiles();
-
-//app.UseRouting();
-
-//app.UseAuthorization();
-
-//app.MapRazorPages();
-
-//app.Run();
