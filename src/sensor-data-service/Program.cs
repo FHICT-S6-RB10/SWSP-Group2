de8b22af4b27082761b1b77ebc3d6fe653e77a5b
Crist Rusnac <c.rusnac@student.fontys.nl>
2022-04-21 19:28:19 +0200

    publish of normalized data to event bus

--- src/sensor-data-service/Program.cs
+++ src/sensor-data-service/Program.cs
@@ -17,11 +17,11 @@ var app = builder.Build();
 //app.UseStaticFiles();
 //app.UseRouting();
 
-//ConnectionFactory cf = new ConnectionFactory();
-//Options opts = ConnectionFactory.GetDefaultOptions();
-//opts.Url = "nats://localhost:4222";
+ConnectionFactory cf = new ConnectionFactory();
+Options opts = ConnectionFactory.GetDefaultOptions();
+opts.Url = "nats://host.docker.internal:4222";
 
-//IConnection c = cf.CreateConnection(opts);
+IConnection c = cf.CreateConnection(opts);
 
 //EventHandler<MsgHandlerEventArgs> h = (sender, args) =>
 //{
@@ -42,6 +42,17 @@ var app = builder.Build();
 //    }
 //};
 
+
+Timer timer = new Timer(TimerCallback, null, 0, 20000);
+
+void TimerCallback(object? state)
+{
+    var request = new Request("sensor_data_service", "technical_health", "heartbeat");
+    var message = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(request));
+    c.Publish("technical_health", message);
+    Console.WriteLine("Message techincal-health-service");
+}
+
 //app.MapGet("/measurements", ([FromServices] MeasurementRepo repo) => {
 //    return repo.GetAll();
 //});
@@ -59,10 +70,14 @@ app.MapPost("/measurements/", async (Measurement measurement) => {
     {
         //send message on event buys
         Console.WriteLine(" normal RR");
+        var request = new RequestData("sensor_data_service", "raw-data-service", measurement);
+        var message = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(request));
+        Console.WriteLine(Encoding.UTF8.GetString(message));
+        //c.Publish("normalized-data", message);
     }
     else
     {
-        Console.WriteLine(" RR to high");
+        Console.WriteLine(" RR too high");
     }
 
     return Results.Created($"/measurements/{measurement.patientId}", measurement);
@@ -93,8 +108,8 @@ public class Measurement
     }
 }
 
-
-
+internal record Request(string origin, string target, string message);
+internal record RequestData(string origin, string target, Measurement message);
 
 
 
