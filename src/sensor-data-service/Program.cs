e6f8e250290dbfb5ab314315bd3fcd0bbff6c9f5
Cristin Rusnac <57223042+dcgws21rki@users.noreply.github.com>
2022-04-22 11:25:49 +0200

    Update Program.cs
    removed comments

--- src/sensor-data-service/Program.cs
+++ src/sensor-data-service/Program.cs
@@ -6,42 +6,14 @@ using System.Text.Json.Serialization;
 using System.Text.Json.Nodes;
 
 var builder = WebApplication.CreateBuilder(args);
-//builder.Services.AddSingleton<MeasurementRepo>();
-
-//var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-
-//builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
-//app.UseHttpsRedirection();
-//app.UseStaticFiles();
-//app.UseRouting();
 
 ConnectionFactory cf = new ConnectionFactory();
 Options opts = ConnectionFactory.GetDefaultOptions();
 opts.Url = "nats://host.docker.internal:4222";
 
 IConnection c = cf.CreateConnection(opts);
-
-//EventHandler<MsgHandlerEventArgs> h = (sender, args) =>
-//{
-//    Console.WriteLine($"Received {args.Message}");
-//    string receivedMessage = Encoding.UTF8.GetString(args.Message.Data);
-//    var deserializedMessage = JsonDocument.Parse(receivedMessage);
-//    var decodedMessage = deserializedMessage.RootElement.GetProperty("message").ToString();
-//    var origin = deserializedMessage.RootElement.GetProperty("origin").ToString();
-
-//    if (decodedMessage.ToLower() == "ping")
-//    {
-//        var reply = args.Message.Reply;
-//        var replyMessage = Encoding.UTF8.GetBytes("th_status_recieved");
-//        c.Publish(reply, replyMessage);
-//        Console.WriteLine($"Published message {Encoding.UTF8.GetString(replyMessage)} to {reply} ");
-//        //client.PostAsync(uri, new StringContent(jsonInString, Encoding.UTF8, "application/json"));
-
-//    }
-//};
-
 
 Timer timer = new Timer(TimerCallback, null, 0, 20000);
 
@@ -52,10 +24,6 @@ void TimerCallback(object? state)
     c.Publish("technical_health", message);
     Console.WriteLine("Message techincal-health-service");
 }
-
-//app.MapGet("/measurements", ([FromServices] MeasurementRepo repo) => {
-//    return repo.GetAll();
-//});
 
 app.MapPost("/measurements/", async (Measurement measurement) => {
 
@@ -68,12 +36,12 @@ app.MapPost("/measurements/", async (Measurement measurement) => {
 
     if (valueRR > 200 && valueRR < 1200)
     {
-        //send message on event buys
+        //send message on event bus
         Console.WriteLine(" normal RR");
         var request = new RequestData("sensor_data_service", "raw-data-service", measurement);
         var message = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(request));
         Console.WriteLine(Encoding.UTF8.GetString(message));
-        //c.Publish("normalized-data", message);
+        c.Publish("normalized-data", message);
     }
     else
     {
@@ -81,7 +49,6 @@ app.MapPost("/measurements/", async (Measurement measurement) => {
     }
 
     return Results.Created($"/measurements/{measurement.patientId}", measurement);
-
 
 });
 
@@ -110,37 +77,4 @@ public class Measurement
 
 internal record Request(string origin, string target, string message);
 internal record RequestData(string origin, string target, Measurement message);
-
-
-
-
-
-
-
-
-//record Measurement(string PatientId, string DeviceId, JsonContent content);
-
-//class MeasurementRepo
-//{
-//    private readonly List<Measurement> _measurements = new List<Measurement>();
-
-//    public void Create(Measurement measurement)
-//    {
-//        if (measurement is null)
-//        {
-//            return;
-//        }
-
-//        _measurements.Add(measurement);
-//    }
-
-//    public List<Measurement> GetByPatientId(string PatientId)
-//    {
-//        return _measurements.FindAll(x => x.PatientId == PatientId);
-//    }
-//    public List<Measurement> GetAll()
-//    {
-//        return _measurements;
-//    }
-//}
 
